d04afaf9d42dffbf17ec1b785ef8f551d4fcd1ef
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-04 11:50:35 +0000

    Add gem & routes

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'rubocop-airbnb'
+  gem 'pry-byebug'
 end
 
 group :test do
@@ -56,7 +58,26 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'rspec-rails'
+  gem "factory_bot_rails"
+  gem 'faker'
+  gem 'gimei'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise'
+
+gem 'bootstrap', '~> 4.5'
+gem 'bootstrap-sass'
+# SaaS用
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+gem "refile-mini_magick"
+gem 'jquery-rails'
+gem 'kaminari','~> 1.2.1'
+gem 'font-awesome-sass', '~> 5.13'
+
+gem 'rails-i18n'
+
+gem 'dotenv-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,43 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # # ========== ユーザー(user) ==========
+  # scope module: :public do
+  #   root to: 'homes#top'
+  #   get 'about' => 'homes#about'
+  #   get 'items/search' => 'items#search'
+
+  #   resources :posts do
+  #     resource :reviews, only: [:create, :destroy]
+  #     resource :likes, only: [:create, :destroy]
+  #     resources :bookmarks, only: [:create, :destroy, :index]
+  #   end
+
+  #   resources :tags, only: [:index, :show]
+
+  #   resources :contacs, only: [:new, :create]
+  #   get 'contacts/complete' => 'contacts#complete'
+
+  #   resources :contacts, only: [:new, :create]
+  #   post 'contacts/confirm' => 'contacts#confirm', as: 'contacts_confirm'
+  #   post 'contacts/back' => 'contacts#back'
+  #   get 'contacts/done' => 'contacts#done'
+
+  #   resources :users, only: [:edit, :update]
+  #   get 'users/my_page' => 'users#show'
+  #   get 'users/unsubscribe' => 'users#unsubscribe'
+  #   patch 'users/withdraw' => 'users#withdraw'
+  # end
+
+  # devise_for :users
+
+  # # ========== 管理者(admin) ==========
+  # devise_for :admins
+
+  # namespace :admin do
+  #   root to: 'homes#top'
+  #   resources :users, only: [:index, :show, :edit, :update]
+  #   resources :contacts, only: [:index, :show]
+  # end
+
 end
