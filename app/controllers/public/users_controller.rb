22b223efc28a0a1ef5b9652d308db6266c4d3601
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-05 05:32:11 +0000

    Edit user_mypege & edit_page

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,12 +1,17 @@
 class Public::UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    if @user.save
+      redirect_to user_path(@user.id)
+    end
   end
 
   def unsubscribe
@@ -14,5 +19,13 @@ class Public::UsersController < ApplicationController
 
   def withdraw
   end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :profile_image, :introduction)
+  end
+
+
 
 end
