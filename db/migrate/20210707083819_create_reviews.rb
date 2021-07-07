57a22496d475902d7c2c2df30467d9696b8ca30c
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-07 23:08:56 +0000

    Add

--- db/migrate/20210707083819_create_reviews.rb
+++ db/migrate/20210707083819_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.text :review, null: false
-      t.text :reply
+      t.integer :reply
       t.references :post, foreign_key: true, null: false
       t.references :user, foreign_key: true, null: false
       t.timestamps
