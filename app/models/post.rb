752cca384e5f23399e35a623434dd49bc0c8e3b1
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-07 08:16:13 +0000

    Create like-ajax

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   attachment :image
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+  
+  def liked_by(user)
+    Like.find_by(user_id: user.id, post_id: id)
+# user_idとpost_idが一致するlikeを検索する
+  end
 end
