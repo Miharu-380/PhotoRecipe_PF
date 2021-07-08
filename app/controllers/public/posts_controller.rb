e01e3ca54c3c3dc6bdcc52486e7c488d82331e97
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-08 13:45:24 +0000

    Change layouts

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -8,10 +8,8 @@ class Public::PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    tag_list = params[:post][:tag_ids].split(' ')
     @post.user_id = current_user.id
     if @post.save
-      @post.save_tag(tag_list)
       redirect_to root_path
       flash[:notice] = "投稿しました"
     else
@@ -20,19 +18,15 @@ class Public::PostsController < ApplicationController
   end
 
   def show
-    @tags = @post.tags
   end
 
   def edit
-    @tag_list =@post.tags.pluck(:tag_name).join(' ')
   end
 
   def update
     @post = Post.find(params[:id])
-    tag_list = params[:post][:tag_ids].split(' ')
     @post.user_id = current_user.id
     if @post.update(post_params)
-      @post.save_tag(tag_list)
       redirect_to post_path(@post)
       flash[:投稿を更新しました]
     else
