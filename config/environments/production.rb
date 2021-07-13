25b7e7a5ae8b29413201825038f0208e0b33ab63
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-13 13:49:08 +0000

    Change production.rb_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp # メールを相手のメールサーバーまで届ける仕組み
+  config.action_mailer.smtp_settings = {
+      port: 587, # メールサーバーの細かい住所的な
+      address: 'smtp.gmail.com',
+      domain: 'smtp.gmail.com',
+      user_name: ENV['SMTP_USERNAME'],
+      password: ENV['SMTP_PASSWORD'],
+      enable_starttls_auto: true # starttls:通信の暗号化を行なう手法
+  }
 end
