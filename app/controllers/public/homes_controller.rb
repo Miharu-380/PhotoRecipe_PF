ff33dffb592fb4323a2b3ab8ab4483698ad057f8
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-19 11:25:43 +0000

    Add jscroll

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,7 +3,7 @@ class Public::HomesController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: "DESC").includes(:user).page(params[:page]).per(25)
     @tag_list = Tag.all
   end
 
