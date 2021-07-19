78be56b7ecba056288f3cfc5a689478770b179c8
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-19 07:24:51 +0000

    Remake routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # ========== ユーザー(user) ==========
   devise_for :users
+
   scope module: :public do
     root to: 'homes#top'
     get 'index' => 'homes#index'
@@ -12,27 +13,33 @@ Rails.application.routes.draw do
       resources :bookmarks, only: [:create, :destroy]
       collection do
         get 'search'
-        get 'm_search' => "posts#mobile_search"
+        get 'm_search' => 'posts#mobile_search'
+        get 'weekly_rank' => 'posts#weekly_rank'
       end
     end
 
-    get '/posts/:id/likes' => "posts#like", as: 'like_index'
-    get '/post/hashtag/:name' => "posts#hashtag"
-    get '/post/weekly_rank' => "posts#weekly_rank"
+    get '/posts/:id/likes' => 'posts#like', as: 'like_index'
+    get '/post/hashtag/:name' => 'posts#hashtag'
 
-    resources :contacts, only: [:new, :create]
-    post 'contacts/confirm' => 'contacts#confirm', as: 'contacts_confirm'
-    post 'contacts/back' => 'contacts#back'
-    get 'contacts/done' => 'contacts#done'
+    resources :contacts, only: [:new, :create] do
+      collection do
+        post 'confirm' => 'contacts#confirm', as: 'contacts_confirm'
+        post 'back' => 'contacts#back'
+        get 'done' => 'contacts#done'
+      end
+    end
 
     resources :users, only: [:show, :edit, :update, :destroy] do
       resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
+      member do
+        get 'unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+        get 'bookmark' => 'users#bookmark', as: 'bookmark'
+      end
     end
-    get 'users/:id/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
-    get 'users/:id/bookmark' => 'users#bookmark', as: 'bookmark'
-    get 'users/:id/timeline' => 'users#timeline', as: 'timeline'
+
+    get 'users/timeline' => 'users#timeline', as: 'timeline'
   end
 
   # # ========== 管理者(admin) ==========
