47a391ebe1c2ffe169b838426215b1f89c0bd8d4
Miharu-380 <38t.ajlkk@gmail.com>
2021-07-26 13:10:17 +0000

    Do Rubocop 2times

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # ========== ユーザー(user) ==========
   devise_for :users, controllers: {
-        sessions: 'public/sessions',
-        registrations: 'public/registrations',
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
   }
 
   devise_scope :user do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Userモデルのテスト', type: :model do
-
   describe 'バリデーションのテスト' do
     subject { user.valid? }
 
