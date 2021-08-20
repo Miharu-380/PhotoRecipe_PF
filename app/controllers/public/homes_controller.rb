c172c0a129972cbf20433e800d949c0a90cc9e3c
Miharu-380 <38t.ajlkk@gmail.com>
2021-08-20 08:25:50 +0000

    Change per_images

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,7 +4,7 @@ class Public::HomesController < ApplicationController
 
   def index
   # 退会済の投稿は非表示
-    @posts = Post.includes(:user).where(users: { is_deleted: false }).order(created_at: :desc).page(params[:page]).per(12)
+    @posts = Post.includes(:user).where(users: { is_deleted: false }).order(created_at: :desc).page(params[:page]).per(20)
     @tag_list = Tag.all
   end
 end
