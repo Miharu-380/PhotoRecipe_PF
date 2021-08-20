09385be2eb8cc30888313a72d4f4a7f84df55349
Miharu-380 <38t.ajlkk@gmail.com>
2021-08-20 08:22:40 +0000

    Add jscroll

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,7 +4,7 @@ class Public::HomesController < ApplicationController
 
   def index
   # 退会済の投稿は非表示
-    @posts = Post.includes(:user).where(users: { is_deleted: false }).order(created_at: :desc)
+    @posts = Post.includes(:user).where(users: { is_deleted: false }).order(created_at: :desc).page(params[:page]).per(12)
     @tag_list = Tag.all
   end
 end
